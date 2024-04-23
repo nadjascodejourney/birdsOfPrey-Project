68ed0d91c3d60a0cd820cd0167dfd20e5c0be2a0
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-04-23 22:45:43 +0200

    added content to json file; map function for didisplaying images for landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 import MainStyle from "../components/MainStyledComponent";
-import jsonData from "../home-content.json";
+import jsonData from "../content.json";
 
 const Home = () => {
-  const { title, subtitle, images, mainText, highlightedText, secondaryTitle } =
-    jsonData.homepage;
+  const {
+    title,
+    subtitle,
+    images,
+    mainText,
+    highlightedText,
+    secondaryTitle,
+    menuImages,
+  } = jsonData.homepage;
 
   return (
     <MainStyle>
@@ -12,14 +19,23 @@ const Home = () => {
       <h2>{subtitle}</h2>
       <figure>
         {images.map((image, index) => (
-          <img key={index} src={image.src} alt={image.alt} />
+          <img key={index} src={image.src} alt={image.alt} /> // map the illus for landingpage
         ))}
       </figure>
       <h3>
         {mainText} <span>{highlightedText}</span>
       </h3>
       <h2>{secondaryTitle}</h2>
-      <figure></figure>
+      <figure>
+        {menuImages.map((image, index) => (
+          <img
+            key={index}
+            src={image.src}
+            alt={image.alt}
+            style={{ width: "25%" }} // change style later
+          /> // map birds for homepage
+        ))}
+      </figure>
     </MainStyle>
   );
 };
